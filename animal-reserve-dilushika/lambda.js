bb7401ef3dd824b26012e518f8aaea48c2d4442f
Thiruni <154052m@uom.lk>
2019-11-07 13:12:02 +0530

    commit3

--- animal-reserve-dilushika/lambda.js
+++ animal-reserve-dilushika/lambda.js
@@ -1,8 +1,9 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 let rekog = new AWS.Rekognition();
 
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     //console.log(JSON.stringify(event, null,2));
     let s3 = event.Records[0].s3;
     rekog.detectLabels({
@@ -14,11 +15,27 @@ exports.handler = function(event, context, callback) {
         },
         MaxLabels: 1
     }).promise()
-    .then(data => {
-        console.log(data);
-        callback(null, {});
-    })
-    .catch(err => callback(err));
+        .then(data => {
+            let lable = data.lables[0].Name;
+            ddb.put({
+                TableName: 'Animal-app-dilushika',
+                Item: {
+                    'name': s3.object.key,
+                    'lable': lable
+                }
+            }).promise()
+                .then((data) => {
+                    callback(null, {});
+                    //your logic goes here
+                })
+                .catch((err) => {
+                    //handle error
+                    callback(err);
+                });
+            console.log(data);
+            
+        })
+        .catch(err => callback(err));
 
-   // callback(null, {"message": "Successfully executed"});
+    // callback(null, {"message": "Successfully executed"});
 }
